679d29715657ecce41f7d8069db8235f9c1adc4b
Mostafa Mohseni Kabir <mostafa_mohsenikabir@yahoo.com>
2019-04-04 13:00:43 +0430

    feat: Refactor code base

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,19 @@ const endsWithOperator = /[*/+-]$/;
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.clearVal = this._clearVal.bind(this)
+    this.typeNumbers = this._typeNumbers.bind(this)
+    this.handleKeyDown = this._handleKeyDown.bind(this)
+    this.performOp = this._performOp.bind(this)
+    this.handleEquals = this._handleEquals.bind(this)
+    this.handleDot = this._handleDot.bind(this)
     this.state = {
       curVal: 0,
       formula: "",
       evaluated: false,
       lastResult: ""
     }
-    this.clearVal = this._clearVal.bind(this);
-    this.typeNumbers = this._typeNumbers.bind(this);
-    this.handleKeyDown = this._handleKeyDown.bind(this);
-    this.performOp = this._performOp.bind(this);
-    this.handleEquals = this._handleEquals.bind(this);
-    this.handleDot = this._handleDot.bind(this);
   }
 
   componentDidMount() {
@@ -42,9 +42,11 @@ class App extends React.Component {
   }
 
   _typeNumbers(e, val) {
+    const { evaluated, curVal, formula } = this.state
+
     // debugger;
     // this.setState({ evaluated: false });
-    if (this.state.evaluated === true) {
+    if (evaluated === true) {
       this.setState({
         curVal: val || e.target.value,
         formula: val || e.target.value,
@@ -52,86 +54,88 @@ class App extends React.Component {
       })
     } else if (
       (val || e.target.value) === 0 &&
-      this.state.curVal === 0 &&
-      this.state.formula === ""
+      curVal === 0 &&
+      formula === ""
     ) {
       this.setState({
         formula: 0
       })
     } else if (
       //if initial val was 0 but i want another digit to start my formula before clicking operator
-      this.state.curVal === 0 &&
-      this.state.formula === 0
+      curVal === 0 &&
+      formula === 0
     ) {
       this.setState({
-        curVal: /(0[.0-9].*)$/.test(this.state.curVal)
-          ? this.state.curVal + (val || e.target.value)
+        curVal: /(0[.0-9].*)$/.test(curVal)
+          ? curVal + (val || e.target.value)
           : val || e.target.value,
-        formula: /(0[.0-9].*)$/.test(this.state.curVal)
-          ? this.state.curVal + (val || e.target.value)
+        formula: /(0[.0-9].*)$/.test(curVal)
+          ? curVal + (val || e.target.value)
           : val || e.target.value
       })
     } else {
       this.setState({
         curVal:
-          this.state.curVal === "0" || isOperator.test(this.state.curVal)
+          curVal === "0" || isOperator.test(curVal)
             ? val || e.target.value
-            : this.state.curVal + (val || e.target.value),
+            : curVal + (val || e.target.value),
         formula:
-          this.state.curVal === "0" && (val || e.target.value) === "0"
-            ? this.state.formula
-            : /([^.0-9]0)$/.test(this.state.formula)
-            ? this.state.formula.slice(0, -1) + (val || e.target.value)
-            : this.state.formula + (val || e.target.value)
+          curVal === "0" && (val || e.target.value) === "0"
+            ? formula
+            : /([^.0-9]0)$/.test(formula)
+            ? formula.slice(0, -1) + (val || e.target.value)
+            : formula + (val || e.target.value)
       })
     }
   }
 
   _handleKeyDown(e) {
-    let { key } = e;
+    let { key } = e
 
     if (/\d/.test(key)) {
-      e.preventDefault();
-      this.typeNumbers(null, key);
+      e.preventDefault()
+      this.typeNumbers(null, key)
     } else if (isOperator.test(key)) {
-      e.preventDefault();
-      this.performOp(key);
+      e.preventDefault()
+      this.performOp(key)
     } else if (key === ".") {
-      e.preventDefault();
-      this.handleDot();
+      e.preventDefault()
+      this.handleDot()
     } else if (key === "Backspace") {
-      e.preventDefault();
-      this.clearVal();
+      e.preventDefault()
+      this.clearVal()
     } else if (key === "Delete") {
-      e.preventDefault();
-      this.clearVal();
+      e.preventDefault()
+      this.clearVal()
     }
     if (key === "Enter") {
-      key = "=";
-      e.preventDefault();
-      this.handleEquals();
-      console.log(key);
+      key = "="
+      e.preventDefault()
+      this.handleEquals()
+      console.log(key)
     }
   }
 
   _performOp(nextOperator) {
-    if (this.state.evaluated) {
+    const { evaluated, formula, lastResult } = this.state
+
+    if (evaluated) {
       this.setState({
-        formula: this.state.lastResult + nextOperator,
+        formula: lastResult + nextOperator,
         curVal: nextOperator,
         evaluated: false
       })
       console.log("performop here? nextOperator");
     } else {
-      if (endsWithOperator.test(this.state.formula)) {
+      if (endsWithOperator.test(formula)) {
         this.setState({
           curVal: nextOperator,
-          formula: this.state.formula.slice(0, -1) + nextOperator
+          formula: formula.slice(0, -1) + nextOperator
         });
       } else {
         this.setState({
           curVal: nextOperator,
-          formula: this.state.formula + nextOperator
+          formula: formula + nextOperator
         })
       }
     }
@@ -155,21 +159,23 @@ class App extends React.Component {
   }
 
   _handleDot() {
-    if (this.state.curVal === 0 && this.state.evaluated) {
+    const { evaluated, curVal, formula } = this.state
+
+    if (curVal === 0 && evaluated) {
       this.setState({
         curVal: "0.",
         formula: "0.",
         evaluated: false
       })
-    } else if (isOperator.test(this.state.curVal)) {
+    } else if (isOperator.test(curVal)) {
       this.setState({
         curVal: "0.",
-        formula: this.state.formula + "0."
+        formula: formula + "0."
       })
-    } else if (!hasDecimal.test(this.state.curVal)) {
+    } else if (!hasDecimal.test(curVal)) {
       this.setState({
-        curVal: this.state.curVal + ".",
-        formula: this.state.formula + "."
+        curVal: curVal + ".",
+        formula: formula + "."
       })
     }
   }
@@ -209,5 +215,6 @@ class App extends React.Component {
     )
   }
 }
+
 
 export default App
--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,146 +1,167 @@
 // modules
 import React from "react"
+import PropTypes from "prop-types"
 
+class Buttons extends React.Component {
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props)
+  }
 
-const Buttons = (clearVal, performOp, typeNumbers, handleDot, handleEquals ) => (
-  <div className="buttons-wrapper">
-    <button
-      id="clear" 
-      className="buttons"
-      value="AC"
-      onClick={clearVal}
-    >
-      AC
-    </button>
-    <button
-      id="divide"
-      className="buttons"
-      value="/"
-      onClick={() => performOp("/")}
-    >
-      /
-    </button>
-    <button
-      id="multiply"
-      className="buttons"
-      value="x"
-      onClick={() => performOp("*")}
-    >
-      x
-    </button>
-    <button
-      id="seven"
-      className="buttons nbr"
-      value="7"
-      onClick={typeNumbers}
-    >
-      7
-    </button>
-    <button
-      id="eight"
-      className="buttons nbr"
-      value="8"
-      onClick={typeNumbers}
-    >
-      8
-    </button>
-    <button
-      id="nine"
-      className="buttons nbr"
-      value="9"
-      onClick={typeNumbers}
-    >
-      9
-    </button>
-    <button
-      id="subtract"
-      className="buttons"
-      value="-"
-      onClick={() => performOp("-")}
-    >
-      -
-    </button>
-    <button
-      id="four"
-      className="buttons nbr"
-      value="4"
-      onClick={typeNumbers}
-    >
-      4
-    </button>
-    <button
-      id="five"
-      className="buttons nbr"
-      value="5"
-      onClick={typeNumbers}
-    >
-      5
-    </button>
-    <button
-      id="six"
-      className="buttons nbr"
-      value="6"
-      onClick={typeNumbers}
-    >
-      6
-    </button>
-    <button
-      id="add"
-      className="buttons"
-      value="+"
-      onClick={() => performOp("+")}
-    >
-      +
-    </button>
-    <button
-      id="one"
-      className="buttons nbr"
-      value="1"
-      onClick={typeNumbers}
-    >
-      1
-    </button>
-    <button
-      id="two"
-      className="buttons nbr"
-      value="2"
-      onClick={typeNumbers}
-    >
-      2
-    </button>
-    <button
-      id="three"
-      className="buttons nbr"
-      value="3"
-      onClick={typeNumbers}
-    >
-      3
-    </button>
-    <button
-      id="zero"
-      className="buttons nbr"
-      value="0"
-      onClick={typeNumbers}
-    >
-      0
-    </button>
-    <button
-      id="decimal"
-      className="buttons"
-      value="."
-      onClick={handleDot}
-    >
-      .
-    </button>
-    <button
-      id="equals"
-      className="buttons"
-      value="="
-      onClick={handleEquals}
-    >
-      =
-    </button>
-  </div>
-)
+  render() {
+    const {
+      clearVal, performOp, typeNumbers, handleDot, handleEquals,
+    } = this.props
+
+    return (
+      <div className="buttons-wrapper">
+        <button
+          id="clear"
+          className="buttons"
+          value="AC"
+          onClick={clearVal}
+        >
+          AC
+        </button>
+        <button
+          id="divide"
+          className="buttons"
+          value="/"
+          onClick={() => performOp("/")}
+        >
+          /
+        </button>
+        <button
+          id="multiply"
+          className="buttons"
+          value="x"
+          onClick={() => performOp("*")}
+        >
+          x
+        </button>
+        <button
+          id="seven"
+          className="buttons nbr"
+          value="7"
+          onClick={typeNumbers}
+        >
+          7
+        </button>
+        <button
+          id="eight"
+          className="buttons nbr"
+          value="8"
+          onClick={typeNumbers}
+        >
+          8
+        </button>
+        <button
+          id="nine"
+          className="buttons nbr"
+          value="9"
+          onClick={typeNumbers}
+        >
+          9
+        </button>
+        <button
+          id="subtract"
+          className="buttons"
+          value="-"
+          onClick={() => performOp("-")}
+        >
+          -
+        </button>
+        <button
+          id="four"
+          className="buttons nbr"
+          value="4"
+          onClick={typeNumbers}
+        >
+          4
+        </button>
+        <button
+          id="five"
+          className="buttons nbr"
+          value="5"
+          onClick={typeNumbers}
+        >
+          5
+        </button>
+        <button
+          id="six"
+          className="buttons nbr"
+          value="6"
+          onClick={typeNumbers}
+        >
+          6
+        </button>
+        <button
+          id="add"
+          className="buttons"
+          value="+"
+          onClick={() => performOp("+")}
+        >
+          +
+        </button>
+        <button
+          id="one"
+          className="buttons nbr"
+          value="1"
+          onClick={typeNumbers}
+        >
+          1
+        </button>
+        <button
+          id="two"
+          className="buttons nbr"
+          value="2"
+          onClick={typeNumbers}
+        >
+          2
+        </button>
+        <button
+          id="three"
+          className="buttons nbr"
+          value="3"
+          onClick={typeNumbers}
+        >
+          3
+        </button>
+        <button
+          id="zero"
+          className="buttons nbr"
+          value="0"
+          onClick={typeNumbers}
+        >
+          0
+        </button>
+        <button
+          id="decimal"
+          className="buttons"
+          value="."
+          onClick={handleDot}
+        >
+          .
+        </button>
+        <button
+          id="equals"
+          className="buttons"
+          value="="
+          onClick={handleEquals}
+        >
+          =
+        </button>
+      </div>
+    )
+  }
+}
+
+Buttons.propTypes = {
+  clearVal: PropTypes.func.isRequired,
+  performOp: PropTypes.func.isRequired,
+  typeNumbers: PropTypes.func.isRequired,
+  handleDot: PropTypes.func.isRequired,
+  handleEquals: PropTypes.func.isRequired,
+}
 
 export default Buttons
